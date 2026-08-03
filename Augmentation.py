5f2822917d2baac967fe4ff8114ffa5b7858a4d5
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-08-03 12:48:28 +0200

    effects list,  effects help

--- Augmentation.py
+++ Augmentation.py
@@ -1,6 +1,7 @@
 import skimage as ski
 import os
 import argparse
+from utils.effects import EffectName
 
 
 def parse_args():
@@ -8,11 +9,12 @@ def parse_args():
         description="Transform an input image in various distinct ways."
     )
     parser.add_argument(
-        "-i", "--input-list", nargs="+", required=True,
+        "-i", "--input-list", nargs="+",
         help="A list of images to process (minimum one)"
     )
     parser.add_argument(
         "-e", "--effects", nargs="+",
+        choices=[effect.name for effect in EffectName],
         help="Choose some effects to apply (instead of ALL by default)"
     )
     parser.add_argument(
@@ -20,6 +22,14 @@ def parse_args():
         help="Print the list of effects available for the -e option"
     )
     args = parser.parse_args()
+    if (not args.input_list and not args.effects_help):
+        parser.error("Either enter a list of input images with -i or "
+                     "ask for the list of available effects with -E")
+    elif (args.effects_help):
+        effect_names = [effect.name for effect in EffectName]
+        print("Available effects:")
+        for effect_name in effect_names:
+            print(f"  {effect_name}")
     return args
 
 
---
+++ utils/effects.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class EffectName(Enum):
+    ZOOM = 0
+    SKEW = 1
+    ROTATION = 2
+    BLUR = 3
+    CONTRAST = 4
+    BRIGHTENSS = 5
